c053b5eac668b86694a67e2222bb8f8491832982
DannyGotTea <danielgallagher500@gmail.com>
2016-04-19 20:17:22 +0100

    Boilerplate OpenCV Code

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ImageFilter
+//
+//  Created by Daniel Gallagher on 19/04/2016.
+//  Copyright (c) 2016 Developer Co. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
